8d8a82447ddad00fe0c8438f1fda3c529c7ef1fd
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2020-07-02 07:36:28 -0300

    Updates

---
+++ precache-manifest.5781928a7917d88ff29a9dabcec5528d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1603765cbf4941fae9606705e238d9",
+    "url": "/landing-vpn/index.html"
+  },
+  {
+    "revision": "18ad8415e7e99cfd77a5",
+    "url": "/landing-vpn/static/js/2.d174988b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/landing-vpn/static/js/2.d174988b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ffa05c1133692f8fbc",
+    "url": "/landing-vpn/static/js/main.1abb7140.chunk.js"
+  },
+  {
+    "revision": "b5d11cb60c41b74c2ad9",
+    "url": "/landing-vpn/static/js/runtime-main.1df51d33.js"
+  },
+  {
+    "revision": "221689fde822a0e109eb2f0f7f20f9b6",
+    "url": "/landing-vpn/static/media/Free.221689fd.png"
+  },
+  {
+    "revision": "135feda1c052fd30afb351bc83668f2c",
+    "url": "/landing-vpn/static/media/Premium.135feda1.png"
+  },
+  {
+    "revision": "51046edbc82fd737a196e7676b3a79ff",
+    "url": "/landing-vpn/static/media/Standard.51046edb.png"
+  },
+  {
+    "revision": "268b35c44a9567b6af2ba718f6816f4b",
+    "url": "/landing-vpn/static/media/amazon.268b35c4.png"
+  },
+  {
+    "revision": "0973c8b64f2ef6bfd5af5635aec2de3c",
+    "url": "/landing-vpn/static/media/discord.0973c8b6.png"
+  },
+  {
+    "revision": "d3b19f4fd96351ce7243722aa7505f87",
+    "url": "/landing-vpn/static/media/feature.d3b19f4f.png"
+  },
+  {
+    "revision": "40b6a3818ed218b5e039b0229d43a82f",
+    "url": "/landing-vpn/static/media/kim.40b6a381.png"
+  },
+  {
+    "revision": "d7a0b8743dcb0e3d848b186cf5e52828",
+    "url": "/landing-vpn/static/media/mac.d7a0b874.png"
+  },
+  {
+    "revision": "252c155eb2295432f06b02a42a90db36",
+    "url": "/landing-vpn/static/media/map.252c155e.png"
+  },
+  {
+    "revision": "a60f57eb16735bc23a03c5f8a1427ea3",
+    "url": "/landing-vpn/static/media/reddit.a60f57eb.png"
+  },
+  {
+    "revision": "4414c66d228967b30b47946d9a2e5808",
+    "url": "/landing-vpn/static/media/spotify.4414c66d.png"
+  },
+  {
+    "revision": "09139267e31cd28b43a8250acb6ca36c",
+    "url": "/landing-vpn/static/media/viezh.09139267.png"
+  },
+  {
+    "revision": "a84fe6954103bd967065c3519353758a",
+    "url": "/landing-vpn/static/media/yessica.a84fe695.png"
+  }
+]);
