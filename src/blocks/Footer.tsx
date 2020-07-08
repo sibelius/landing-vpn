6e8759980f1658f9e967668eb42c509ac0321c93
Laura <laurabeatriserafim@gmail.com>
2020-07-08 07:56:52 -0300

    refactor(footer): apply grid

--- src/blocks/Footer.tsx
+++ src/blocks/Footer.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Flex } from 'rebass';
+import { Box } from 'reflexbox';
 import styled from 'styled-components';
+import { space, SpaceProps } from 'styled-system';
+
 import LogoIcon from '../icons/LogoIcon';
 import { Bold, Medium, Regular } from '../ui/Text';
 import FacebookIcon from '../icons/FacebookIcon';
 import TwitterIcon from '../icons/TwitterIcon';
 import InstagramIcon from '../icons/InstagramIcon';
-import { space } from 'styled-system';
 
-const FooterLink = styled.a`
+const FooterLink = styled.a<SpaceProps>`
   font-weight: 400;
   color: #4f5665;
   cursor: pointer;
+  font-size: 16px;
+
   &:hover {
     text-decoration: underline;
   }
+
   ${space}
 `;
 
@@ -25,7 +30,13 @@ const IconLink = styled.a`
 
 const Footer = () => {
   return (
-    <Flex flex={1} mt="60px" justifyContent="space-between">
+    <Flex
+      flex={1}
+      mt="60px"
+      padding="40px"
+      justifyContent="space-between"
+      flexDirection={['column', 'row']}
+    >
       <Flex flexDirection="column">
         <Flex flexDirection="row" alignItems="center">
           <LogoIcon mr="10px" />
@@ -52,58 +63,36 @@ const Footer = () => {
           ©2020Lasles<Medium>VPN</Medium>
         </Regular>
       </Flex>
-      <Flex>
-        <Flex flexDirection="column">
-          <Medium fontSize="18px">Product</Medium>
-          <FooterLink fontSize="16px" mt="20px">
-            Download
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            Pricing
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            Locations
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            Server
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            Countries
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            Blog
-          </FooterLink>
-        </Flex>
-        <Flex flexDirection="column" ml="120px">
-          <Medium fontSize="18px">Engage</Medium>
-          <FooterLink fontSize="16px" mt="20px">
-            LaslesVPN ?
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            FAQ
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            Tutorials
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            About Us
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            Privacy Policy
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            Terms of Service
-          </FooterLink>
-        </Flex>
-        <Flex flexDirection="column" ml="120px">
-          <Medium fontSize="18px">Earn Money</Medium>
-          <FooterLink fontSize="16px" mt="20px">
-            Affiliate
-          </FooterLink>
-          <FooterLink fontSize="16px" mt="10px">
-            Become Partner
-          </FooterLink>
-        </Flex>
+      <Flex mt={['40px', 0]}>
+        <Box width={[1, 1 / 3]}>
+          <Flex flexDirection="column">
+            <Medium fontSize="18px">Product</Medium>
+            <FooterLink mt="20px">Download</FooterLink>
+            <FooterLink mt="10px">Pricing</FooterLink>
+            <FooterLink mt="10px">Locations</FooterLink>
+            <FooterLink mt="10px">Server</FooterLink>
+            <FooterLink mt="10px">Countries</FooterLink>
+            <FooterLink mt="10px">Blog</FooterLink>
+          </Flex>
+        </Box>
+        <Box width={[1, 1 / 3]} mx={[40, 20, 80]}>
+          <Flex flexDirection="column">
+            <Medium fontSize="18px">Engage</Medium>
+            <FooterLink mt="20px">LaslesVPN ?</FooterLink>
+            <FooterLink mt="10px">FAQ</FooterLink>
+            <FooterLink mt="10px">Tutorials</FooterLink>
+            <FooterLink mt="10px">About Us</FooterLink>
+            <FooterLink mt="10px">Privacy Policy</FooterLink>
+            <FooterLink mt="10px">Terms of Service</FooterLink>
+          </Flex>
+        </Box>
+        <Box width={[1, 1 / 3]}>
+          <Flex flexDirection="column">
+            <Medium fontSize="18px">Earn Money</Medium>
+            <FooterLink mt="20px">Affiliate</FooterLink>
+            <FooterLink mt="10px">Become Partner</FooterLink>
+          </Flex>
+        </Box>
       </Flex>
     </Flex>
   );
