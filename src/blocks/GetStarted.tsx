0fb57f8a3042813aa087a64ae176425525f590d2
Laura <laurabeatriserafim@gmail.com>
2020-07-08 09:27:15 -0300

    fix(block): fix padding and image width of getStartedBlock

--- src/blocks/GetStarted.tsx
+++ src/blocks/GetStarted.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { Flex } from 'rebass';
 import styled from 'styled-components';
 import { space } from 'styled-system';
-import macImg from '../img/mac.png';
+import media from 'styled-media-query';
+
 import { Bold, Medium, Regular } from '../ui/Text';
+import macImg from '../img/mac.png';
 
 const Img = styled.img`
-  width: 600px;
+  width: 50%;
   height: 380px;
+
+  ${media.lessThan('medium')`
+    width: 100%;
+    height: 100%;
+    margin-top: 40px;
+  `}
 `;
 
 export const GetStartedButton = styled.button`
   width: 250px;
   height: 60px;
-  background-color: #F53838;
+  background-color: #f53838;
   border: 0;
   cursor: pointer;
   border-radius: 10px;
@@ -29,28 +37,37 @@ export const BoxShadow = styled.div`
   border-radius: 10px;
   top: -30px;
   left: 20px;
-`
+`;
+
+export const Section = styled(Flex)`
+  padding: 20px;
+
+  ${media.lessThan('medium')`
+    padding: 40px;
+    flex-direction: column;
+    margin: 0;
+  `}
+`;
 
 const GetStarted = () => {
   return (
-    <Flex mt='80px' justifyContent='space-between'>
-      <Flex flexDirection='column'>
-        <Medium
-          fontSize='48px'
-          color='#0B132A'
-          lineHeight='70px'
-        >Want anything to be easy with <Bold>LaslesVPN.</Bold></Medium>
-        <Regular mt='20px'>
-          Provide a network for all your needs with ease and fun using <Bold>LaslesVPN</Bold> discover interesting features from us.
+    <Section mt="48px" justifyContent="space-between">
+      <Flex flexDirection="column">
+        <Medium fontSize="48px" color="#0B132A" lineHeight="70px">
+          Want anything to be easy with <Bold>LaslesVPN.</Bold>
+        </Medium>
+        <Regular mt="20px">
+          Provide a network for all your needs with ease and fun using{' '}
+          <Bold>LaslesVPN</Bold> discover interesting features from us.
         </Regular>
-        <GetStartedButton mt='50px'>
-          <Bold color='#ffffff'>Get Started</Bold>
+        <GetStartedButton mt="50px">
+          <Bold color="#ffffff">Get Started</Bold>
         </GetStartedButton>
         <BoxShadow />
       </Flex>
       <Img src={macImg} />
-    </Flex>
+    </Section>
   );
-}
+};
 
 export default GetStarted;
