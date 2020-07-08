ba726981cbf3f21ec8f66f5c905d50021ea1a2e1
Laura <laurabeatriserafim@gmail.com>
2020-07-08 09:11:25 -0300

    refactor(sponsored): apply flexbox to be responsive

--- src/blocks/Sponsored.tsx
+++ src/blocks/Sponsored.tsx
@@ -7,20 +7,37 @@ import Discord from '../img/discord.png';
 import Netflix from '../img/netflix.png';
 import Reddit from '../img/reddit.png';
 import Spotify from '../img/spotify.png';
+import media from 'styled-media-query';
 
 const Img = styled.img`
   ${layout}
 `;
 
+export const Container = styled(Flex)`
+  max-width: 100%;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 0 20px;
+
+  ${media.lessThan('medium')`
+    flex-direction: column;
+
+    img {
+      margin: 20px 0;
+    }
+  `};
+`;
+
 const Sponsored = () => {
   return (
-    <Flex alignItems="center" justifyContent="space-between">
+    <Container alignItems="center">
       <Img src={Netflix} width="200px" height="112px" />
       <Img src={Reddit} width="150px" height="50px" />
       <Img src={Amazon} width="277px" height="208px" />
       <Img src={Discord} width="187px" height="63px" />
       <Img src={Spotify} width="171px" height="51px" />
-    </Flex>
+    </Container>
   );
 };
 
